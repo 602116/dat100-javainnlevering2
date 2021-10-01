352200d2bc2748d863286e4494187f0c65264887
602116 <602116@stud.hvl.no>
2021-10-01 12:12:22 +0200

    Oppdatert metoder.

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -2,49 +2,98 @@ package no.hvl.dat100.lab6.matriser;
 
 public class Matriser {
 
-	// a)
-	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-	}
+    // a)
+    public static void skrivUt(int[][] matrise) {
 
-	// b)
-	public static String tilStreng(int[][] matrise) {
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                System.out.print(matrise[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
-	}
+    // b)
+    public static String tilStreng(int[][] matrise) {
 
-	// c)
-	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
-	}
 
-	// d)
-	public static boolean erLik(int[][] a, int[][] b) {
+        if (matrise == null)
+            return "null";
+        StringBuilder tabellS = new StringBuilder();
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                tabellS.append(matrise[i][j] + " ");
 
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
-	}
-	
-	// e)
-	public static int[][] speile(int[][] matrise) {
+            }
+            tabellS.append('\n');
 
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
-	
-	}
 
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
+        }
+        return tabellS.toString();
 
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
-	}
+
+    }
+
+    // c)
+    public static int[][] skaler(int tall, int[][] matrise) {
+
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+
+                matrise[i][j] = matrise[i][j] * tall;
+
+            }
+
+        }
+        return matrise;
+
+    }
+
+    // d)
+    public static boolean erLik(int[][] a, int[][] b) {
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+
+                if (a[i][j] != b[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // e)
+    public static int[][] speile(int[][] matrise) {
+
+        int[][] matriseSpeil = new int[matrise.length][matrise[0].length];
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+
+                matriseSpeil[j][i] = matrise[i][j];
+            }
+
+        }
+        return matriseSpeil;
+    }
+
+    // f)
+    public static int[][] multipliser(int[][] a, int[][] b) {
+        int[][] multi = new int[a.length][b.length];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                for (int k = 0; k < a.length; k++) {
+
+                    multi[i][j] += (a[i][k] * b[k][j]);
+
+                }
+
+            }
+
+        }
+        return multi;
+
+
+    }
 }
